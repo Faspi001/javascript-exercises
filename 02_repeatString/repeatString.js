95a524ac78b37665a1d465f5dd71bdeb34e184a9
faspi001 <snnoox@gmail.com>
2025-01-15 17:08:09 +0100

    Task 1 & 2 erledigt

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let i =0;
+    let string = ''
+    if(times <0){
+        return "ERROR"
+    }
+    while(i<times){
+        string+=word
+        i++;
+    }
+    return string;
 };
 
 // Do not edit below this line
